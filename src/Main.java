fc183a986e6a8091027dcce015bfa72034b102ad
AndreyAndrianov jdplm-32 <darkc0lder@mail.ru>
2023-01-04 22:34:28 +0300

    first main commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Stream;
 
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        Stream<Integer> stream = intList.stream();
-        intList.stream().filter(x -> x > 0).filter(x -> x % 2 == 0).sorted(Comparator.naturalOrder()).forEach(System.out::println);
+        intList.stream().filter(x -> x > 0).filter(x -> x % 2 == 0).sorted(Comparator.naturalOrder()).forEach(System.out::print);
+
+        System.out.println();
+
+        Collections.sort(intList);
+        for (int i : intList) {
+            if (i > 0) {
+                if (i % 2 == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+
     }
 }
